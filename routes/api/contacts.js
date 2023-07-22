e8f42e4f5e6bb9cbe34126a52bf19d33654eb8c2
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-07-22 12:04:39 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { validateBody } from '../../decorators/index.js';
 import schemas from '../../schemas/contacts.js';
 import contactsController from '../../controllers/contacts.js';
+import isValidId from '../../middlewars/isValidId.js';
 
 const router = express.Router()
 
@@ -11,8 +12,10 @@ router.get('/:id', contactsController.getById);
 
 router.post('/', validateBody(schemas.addSchema), contactsController.add);
 
-router.put('/:id', validateBody(schemas.addSchema), contactsController.updateById);
+router.put('/:id', isValidId, validateBody(schemas.addSchema), contactsController.updateById);
 
-router.delete('/:id', contactsController.deleteByid);
+router.patch("/:id/favorite", isValidId, validateBody(schemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact);
+
+router.delete('/:id', isValidId, contactsController.deleteByid);
 
 export default router;
